ad1ea30818a9632dfb3881981e3949a601ae361d
destro014 <japmc11@gmail.com>
2016-07-16 20:04:10 +0545

    Added a simple game

---
+++ input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter any number"<<endl;
+    cin >> a;
+    cout << "the enter number is:" << a <<endl
+    ;
+    return 0;
+}

860efc91f03ac49866a6cd6483e666ee1df710bc
Destro <japmc11@gmail.com>
2016-11-10 17:01:18 +0545

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+/*program to calculate interest of given principle amount*/
+#include<iostream>
+using namespace std;
+int main(){
+    int p,r,t,i;
+    cout<<"Enter the principal:";
+    cin>>p;
+    cout<<"\nEnter the time in years:";
+    cin>>t;
+    cout<<"\nEnter the rate:";
+    cin>>r;
+    i=p*t*t/100;
+    cout<<"The interest of "<<p<<" in "<<t<<" years at the rate of "<<r<<" is:"<<i<<endl;
+    return 0;
+}

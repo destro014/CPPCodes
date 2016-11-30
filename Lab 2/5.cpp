0f44cd39547e7bf0726734093b78b3892f0598df
Destro <japmc11@gmail.com>
2016-11-30 19:51:58 +0545

    :100:

--- Lab 2/5.cpp
+++ Lab 2/5.cpp
@@ -1,1 +1,27 @@
 //program to input student details like name, roll no, faculty, semester and display the records by using function.
+#include<iostream>
+using namespace std;
+int display(char [],int,char [],int);
+int main(){
+    char name[30],faculty[10];
+    int roll,sem;
+    cout<<"Enter the following details of the students."<<endl;
+    cout<<"Enter the name of the student:";
+    cin>>name;
+    cout<<"\nRoll No.:";
+    cin>>roll;
+    cout<<"\nFaculty:";
+    cin>>faculty;
+    cout<<"\nSemester:";
+    cin>>sem;
+    display(name,roll,faculty,sem);
+    return 0;
+}
+int display(char name[],int roll ,char faculty[] ,int sem){
+    cout<<"The details of the student are:"<<endl;
+    cout<<"Name:"<<name<<endl;
+    cout<<"Roll No.:"<<roll<<endl;
+    cout<<"Faculty:"<<faculty<<endl;
+    cout<<"Semester:"<<sem<<endl;
+    return 0;
+}

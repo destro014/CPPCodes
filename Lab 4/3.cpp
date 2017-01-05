64e211725680a5b46a069e025d11b55e16c21cd0
Destro <japmc11@gmail.com>
2017-01-05 21:08:00 +0545

    display the count value of class

--- Lab 4/3.cpp
+++ Lab 4/3.cpp
@@ -1,1 +1,22 @@
 /*wap to display the count value of class. use multiple object to display value with the concept of static data*/
+#include<iostream>
+using namespace std;
+class counter{
+    static int count;
+public:
+    void getdata(){
+        count++;
+    }
+    void display(){
+        cout<<"Counter:"<<count<<endl;
+    }
+};
+int counter::count;
+int main(){
+    counter c1,c2;
+    c1.getdata();
+    c1.display();
+    c2.getdata();
+    c2.display();
+    return 0;
+}

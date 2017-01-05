90393feadfcfefba9a3d47ec1d60be3a1bc122af
Destro <japmc11@gmail.com>
2017-01-05 21:08:45 +0545

    convert length

---
+++ Lab 5/1.cpp
@@ -0,0 +1,34 @@
+/*WAP to convert length into inches.
+Set the length in feet and inch format through  constructor.
+Display the output form other object by returning objects*/
+#include<iostream>
+using namespace std;
+/*class convert{
+    int lf,li;
+public:
+    convert(int feet,int inch){
+        lf=feet;
+        li=inch;
+    }
+    convert calculate(){
+        convert tmp;
+        tmp.li=li+(lf*12);
+        return(tmp);
+    }
+    void display(){
+        cout<<"Length in inch is:"<<li<<endl;
+    }
+};
+int main(){
+    int lf,li;
+    cout<<"Enter the length in feet:";
+    cin>>lf;
+    cout<<"Enter the length in inch:";
+    cin>>li;
+    convert l2;
+    convert l3(lf,li);
+    l2=l3.calculate();
+    l2.display();
+    return 0;
+}
+*/

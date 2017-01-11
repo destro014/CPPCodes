02b302a035f2cd1679872554468df879900d4ae3
Destro <japmc11@gmail.com>
2017-01-11 21:32:31 +0545

    Program to set counter value from constructor

--- Lab 5/3.cpp
+++ Lab 5/3.cpp
@@ -1,1 +1,18 @@
 /*wap to set counter value from constructor and increment that counter to display incremented value */
+#include<iostream>
+using namespace std;
+class counter{
+    int count;
+public:
+    counter(int a){
+        count=a;
+    }
+    int increment(){
+        count++;
+        return count;
+    }
+};
+int main(){
+    counter a(10);
+    cout<<"The incremented value is:"<<a.increment();
+}
